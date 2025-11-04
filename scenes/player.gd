910f2800d0b06876b05fd6836bada99439fcacc7
Doug White <doug.white@versatechit.com.au>
2025-11-05 09:43:16 +1100

    Small art asset fixes

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
