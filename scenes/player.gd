357f32d668ef358bdbce8c00dacdd13c7b3bad00
Doug White <doug.white@versatechit.com.au>
2025-11-05 13:01:00 +1100

    Added idle / walk animations for Harry for all 4 directions

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,19 +1,46 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 const SPEED = 300.0
+var last_dir = 2 	# 0 - right, 1 - left, 2 - down, 3 - up
 
 func _physics_process(_delta):
 
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	direction = Input.get_axis("ui_up", "ui_down")
+	direction = Input.get_axis("move_up", "move_down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	if velocity.length() > 0:
+		if velocity.x > 0:
+			animated_sprite.play("walk_right")
+			last_dir = 0
+		elif velocity.x < 0:
+			animated_sprite.play("walk_left")
+			last_dir = 1
+		elif velocity.y > 0:
+			animated_sprite.play("walk_down")
+			last_dir = 2
+		elif velocity.y < 0:
+			animated_sprite.play("walk_up")
+			last_dir = 3
+		
+	else:
+		if last_dir == 0:
+			animated_sprite.play("idle_right")
+		elif last_dir == 1:
+			animated_sprite.play("idle_left")
+		elif last_dir == 2:
+			animated_sprite.play("idle_down")
+		elif last_dir == 3:
+			animated_sprite.play("idle_up")
+			
 	move_and_slide()
