bd57c4b6b5b4e5cf3f7e181f9cd29d268e0588e5
Doug White <doug.white@versatechit.com.au>
2025-11-19 23:29:25 +1100

    Make radio tower tuning tutorial much more forgiving

--- scripts/tunables/radio_tower.gd
+++ scripts/tunables/radio_tower.gd
@@ -14,7 +14,7 @@ func _process(delta):
 	# If we are waiting for the player to tune the tower
 	if not tuned_tower:
 		# and they ARE tuning the tower, keep track of how long for
-		if game_manager.current_frequency == self.band:
+		if abs(game_manager.current_frequency - self.band) <= 2:
 			tuned_elapsed += delta
 			# once they've tuned long enough, tell the narrative manager
 			if tuned_elapsed >= TUNED_TIMER:
